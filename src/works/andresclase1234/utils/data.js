425ec47ab76493079672d37b4fb404af2f24100a
Andres <ab.cdr.clase@gmail.com>
2020-10-20 16:59:17 +0200

    Data are now in utils/data ( working)

--- src/works/andresclase1234/utils/data.js
+++ src/works/andresclase1234/utils/data.js
@@ -4,3 +4,10 @@ export const dates = [
   { id: 3, name: 'Eva', holidayFrom: new Date('2020-12-15'), holidayTo: new Date('2020-12-22') },
   { id: 4, name: 'Luis', holidayFrom: new Date('2020-10-19'), holidayTo: new Date('2020-10-23') }
 ];
+export const stat = ['Pendiente de aprobación', 'Aprobado', 'No aprobado'];
+export const data = [
+  { id: 1, name: 'Andrés Blanco', request: '16/09/20', start: '15/10/20', finish: '20/10/20', state: stat[0], stateD: '16/09/20', arrState: stat },
+  { id: 2, name: 'Mario Lopez', request: '17/09/20', start: '21/10/20', finish: '25/10/20', state: stat[1], stateD: '17/09/20', arrState: stat },
+  { id: 3, name: 'Karina Bueno', request: '18/09/20', start: '22/10/20', finish: '30/10/20', state: stat[1], stateD: '18/09/20', arrState: stat },
+  { id: 3, name: 'Eva Fernandez', request: '19/09/20', start: '23/10/20', finish: '01/11/20', state: stat[1], stateD: '19/09/20', arrState: stat }
+];
