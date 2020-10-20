3789bc448e730178fd2cea6e623e7f74b8aef06c
Andres <ab.cdr.clase@gmail.com>
2020-10-20 16:16:15 +0200

    Data is now in utils/data file

--- src/works/andresclase1234/andresclase1234-page.js
+++ src/works/andresclase1234/andresclase1234-page.js
@@ -1,18 +1,11 @@
 import { LitElement, html } from 'lit-element';
 import { commonStyles } from '../../utils/custom-styles';
+import { dates } from './utils/data';
 import '../../components/common-header';
 import '../../components/work-header';
+import './empleado-vacaciones/solicitud-vacaciones';
 import './admin-vacaciones/admin-vacaciones';
-
-const stat = ['Pendiente de aprobación', 'Aprobado', 'No aprobado'];
-const data = [
-  { id: 1, name: 'Andrés Blanco', request: '16/09/20', start: '15/10/20', finish: '20/10/20', state: stat[0], stateD: '16/09/20', arrState: stat },
-  { id: 2, name: 'Mario Lopez', request: '17/09/20', start: '21/10/20', finish: '25/10/20', state: stat[1], stateD: '17/09/20', arrState: stat },
-  { id: 3, name: 'Karina Bueno', request: '18/09/20', start: '22/10/20', finish: '30/10/20', state: stat[1], stateD: '18/09/20', arrState: stat },
-  { id: 4, name: 'Juan Sanchez', request: '18/10/20', start: '30/10/20', finish: '18/11/20', state: stat[2], stateD: '18/10/20', arrState: stat },
-  { id: 5, name: 'Maria Fernandez', request: '18/11/20', start: '19/11/20', finish: '20/12/20', state: stat[2], stateD: '18/11/20', arrState: stat },
-  { id: 6, name: 'Eva Prieto', request: '18/12/20', start: '24/11/20', finish: '10/12/20', state: stat[0], stateD: '18/12/20', arrState: stat }
-];
+import './info-holidays/info-holidays';
 
 class Andresclase1234Page extends LitElement {
   static get styles() {
@@ -27,7 +20,7 @@ class Andresclase1234Page extends LitElement {
       <section class="container">
         <work-header>Andresclase1234</work-header>
       </section>
-      <admin-vacaciones .list="${data}" .nElements="${2}"></admin-vacaciones>
+      <info-holidays .list="${dates}" .nElements="${2}"></info-holidays>
     `;
   }
 }
---
+++ src/works/andresclase1234/utils/data.js
@@ -0,0 +1,6 @@
+export const dates = [
+  { id: 1, name: 'Andres', holidayFrom: new Date('2020-10-15'), holidayTo: new Date('2020-10-19') },
+  { id: 2, name: 'Juan', holidayFrom: new Date('2020-11-15'), holidayTo: new Date('2020-11-20') },
+  { id: 3, name: 'Eva', holidayFrom: new Date('2020-12-15'), holidayTo: new Date('2020-12-22') },
+  { id: 4, name: 'Luis', holidayFrom: new Date('2020-10-19'), holidayTo: new Date('2020-10-23') }
+];
