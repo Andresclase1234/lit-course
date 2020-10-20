e994eea8e89ed05d6e0c72dc1f810c606d65275b
Andres <ab.cdr.clase@gmail.com>
2020-10-20 16:25:51 +0200

    Updated

--- src/works/andresclase1234/andresclase1234-page.js
+++ src/works/andresclase1234/andresclase1234-page.js
@@ -3,9 +3,11 @@ import { commonStyles } from '../../utils/custom-styles';
 import { dates } from './utils/data';
 import '../../components/common-header';
 import '../../components/work-header';
-import './empleado-vacaciones/solicitud-vacaciones';
 import './admin-vacaciones/admin-vacaciones';
-import './info-holidays/info-holidays';
+
+const components = {
+  adminVacaciones: () => html`<admin-vacaciones list="${dates}"></admin-vacaciones>`
+};
 
 class Andresclase1234Page extends LitElement {
   static get styles() {
@@ -14,14 +16,34 @@ class Andresclase1234Page extends LitElement {
     ];
   }
 
+  static get properties() {
+    return {
+      current: { type: String, attribute: false }
+    };
+  }
+
+  constructor() {
+    super();
+    this.current = 'adminVacaciones';
+  }
+
+  setComponent(component) {
+    this.current = component;
+  }
+
   render() {
     return html`
-      <common-header></common-header>
-      <section class="container">
-        <work-header>Andresclase1234</work-header>
-      </section>
-      <info-holidays .list="${dates}" .nElements="${2}"></info-holidays>
-    `;
+        <common-header></common-header>
+        <section class="container">
+          <work-header>andresclase1234</work-header>
+          <div class="common-list">
+            ${Object.keys(components).map(item => html`
+              <button class="common-btn" @click="${() => this.setComponent(item)}">${item}</button>
+            `)}
+          </div>
+          ${components[this.current]()}
+        </section>
+      `;
   }
 }
 
